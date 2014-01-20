feb3ee69b9a747ad86ed9dc6bd4309d0f7b8a970
Ben Chatelain <ben.d.chatelain@kp.org>
2014-01-20 09:55:35 -0700

    Shorter .dmg URLs with no params

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@
 class java {
   include boxen::config
 
-  $jre_url = 'http://sdlc-esd.sun.com/ESD6/JSCDL/jdk/7u51-b13/jre-7u51-macosx-x64.dmg?AuthParam=1390235074_3eef9964bc12c9e711a238ff30a33360&GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/7u51-b13/jre-7u51-macosx-x64.dmg&File=jre-7u51-macosx-x64.dmg&BHost=javadl.sun.com'
-  $jdk_url = 'http://sdlc-esd.sun.com/ESD6/JSCDL/jdk/7u51-b13/jdk-7u51-macosx-x64.dmg?AuthParam=1390235074_3eef9964bc12c9e711a238ff30a33360&GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/7u51-b13/jdk-7u51-macosx-x64.dmg&File=jdk-7u51-macosx-x64.dmg&BHost=javadl.sun.com'
+  $jre_url = 'http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jre-7u51-macosx-x64.dmg'
+  $jdk_url = 'http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-macosx-x64.dmg'
   $wrapper = "${boxen::config::bindir}/java"
   $sec_dir = '/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/jre/lib/security'
 
